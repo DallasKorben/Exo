b5984368e4b35cdadc6f5aa576f8c6f850aa4000
PeM <pe.m@free.fr>
2019-10-20 15:32:19 +0100

    ajout d'une fonction 'distribue_cartes'

--- fonction.py
+++ fonction.py
@@ -15,3 +15,46 @@ def parier():
             pari = compo_pari
         else:
             print("Montant invalide, il ne vous reste que " + str(jetons) + " jetons")
+
+
+def distribue_cartes():
+    ''' Cette fonction distribue les cartes et mets en place un tour de jeu '''
+
+    # Préparer toutes les variables globales
+    global resultat, jouer, paquet, main_joueur, main_banque, jetons, pari
+
+    # Crée le paquet
+    paquet = Paquet()
+
+    # Le mélanger
+    paquet.mélanger()
+
+    # Faire un pari
+    parier()
+
+    # Préparer les mains du joueur et de la banque
+    main_joueur = Main()
+    main_banque = Main()
+
+    # Distribuer les premières cartes
+    main_joueur.ajoute_carte(paquet.donne())
+    main_joueur.ajoute_carte(paquet.donne())
+
+    main_banque.ajoute_carte(paquet.donne())
+    main_banque.ajoute_carte(paquet.donne())
+
+    resultat = "(J)oue ou (P)asse? tapez J ou P : "
+    if main_joueur.calc_val() == 21:
+        resultat = "BLACK JACK ! 21 !" + rejouer_phrase
+        jetons += 1.5 * pari
+        jouer = False
+        print("Total de jetons : " + str(jetons))
+
+    if jouer == True:
+        print('Je me couche, désolé')
+        jetons -= pari
+
+
+    # Mise en place pour avoir la main en cours de jeu
+    jouer = True
+    tour_de_jeu()
